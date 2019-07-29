975b511edde30181cdcd2dec61fc4cee269791e5
Fran <asensi.benito@gmail.com>
2019-07-29 09:40:35 +0200

    Rellenar lista de apartamentos

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import config as cfc
 import csv
+from apartment import *
 
-with open(cfc.csvpath) as csvfile:
-    reader = csv.reader(csvfile, delimiter=';')
+def main():
+    miApartamento = None
+    listaApartamentos = []
 
-    for row in reader:
-        print(row[0]) # ID
-        print(row[40]) # Cód postal
-        print(row[39]) # Provincia
-        print(row[38]) # Ciudad
-        print(row[43]) # Cód País
-        print(row[44]) # Nombre País
-        print(row[45]) # Latitud
-        print(row[46]) # Longitud
+    with open(cfc.csvpath) as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+
+        for row in reader:
+            miApartamento = Apartment(pId = row[0], pCodPosta = row[40], pProvincia = row[39], pCiudad = row[38], 
+                pCodPais = row[43], pNombrePais = row[44], pLatitud = row[45], pLongitud = row[45])
+
+            listaApartamentos.append(miApartamento)
+
+if __name__ == "__main__":
+    main()
         

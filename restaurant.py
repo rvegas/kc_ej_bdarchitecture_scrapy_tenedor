330438c0a177f89bec0114ff3d2bd86d6a40fae1
Fran <asensi.benito@gmail.com>
2019-08-02 14:27:59 +0200

    Obtención datos primera página

--- restaurant.py
+++ restaurant.py
@@ -20,8 +20,16 @@ class Restaurant(scrapy.Spider):
         
     def empieza(self):
         process = CrawlerProcess({
-            'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+            'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+            'DOWNLOAD_DELAY': 0.25,
+            'COOKIES_ENABLED': 'False'
         })
+
+        #process = CrawlerProcess({
+        #    'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.87 Safari/537.36',
+        #    'DOWNLOAD_DELAY': 0.25,
+        #    'COOKIES_ENABLED': 'False'
+        #})
 
         process.crawl(Restaurant)
         process.start()
@@ -29,8 +37,28 @@ class Restaurant(scrapy.Spider):
     def parse(self, response):
         if response.url == 'https://www.eltenedor.es/':
             return
+
+        for result in response.css('li.resultItem'):
+            id = result.css('li.resultItem::attr(data-restaurant-id)').extract_first()
             
-        print(response.url)
-        #for result in response.css('li.resultItem'):
-            #id = result.css('data-restaurant-id ::text').extract_first()
-            #print(id)
+            for info in result.css('div.resultItem-information h3'):
+                url = info.css('a::attr(href)').extract_first()
+                nombre = info.css('a::text').extract_first()
+
+            direccion = self.trataCadenas(result.css('div.resultItem-address::text').extract_first())
+
+            precioMedio = self.trataCadenas(result.css('div.resultItem-averagePrice::text').extract_first())
+
+            valoracion = result.css('span.rating-ratingValue::text').extract_first()
+
+            print('Id:',id,'Nombre:',nombre,'Url:',url, 'Dirección:', direccion, 'Precio medio:', precioMedio, 'Valoración:',valoracion)
+
+        for next_page in response.css('li.next'):
+            siguiente = next_page.css('a::attr(href)').extract_first()
+            print(siguiente)
+
+    def trataCadenas(self, pCadena):
+        pCadena = pCadena.replace('\n', '')
+        pCadena = pCadena.strip()
+
+        return pCadena
